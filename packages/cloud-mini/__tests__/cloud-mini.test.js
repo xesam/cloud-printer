e0c8ed582ecbbddcf4c06d2517f07744f2c5149b
xesam <xesamguo@gmail.com>
2022-11-11 18:15:18 +0800

    tests /fix

--- packages/cloud-mini/__tests__/cloud-mini.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const cloudMini = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(cloudMini(), 'Hello from cloudMini');
-console.info("cloudMini tests passed");
